9e86927b08b93bb26b42641d4c7324fde0d6c60f
XOneFG <117354752+VsrStudio@users.noreply.github.com>
2024-11-22 16:26:05 +0700

    Create Main.php

---
+++ src/VsrStudio/MusicUI/Main.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace VsrStudio\MusicUI;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\player\Player;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class Main extends PluginBase implements Listener {
+
+    protected function onEnable(): void {
+        $this->getLogger()->info("MusicUI is enabled!");
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        if ($command->getName() === "musicui") {
+            if (!$sender instanceof Player) {
+                $sender->sendMessage("This command can only be used by players.");
+                return true;
+            }
+
+            $form = new MusicForm();
+            $sender->sendForm($form->getMusicForm());
+            return true;
+        }
+        return false;
+    }
+}
